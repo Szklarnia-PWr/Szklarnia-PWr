10a50942193c06fa0fe349723adb295521601ea3
Maciej Opaliński <poprostumieciek@gmail.com>
2022-09-27 20:09:06 +0200

    feat(api): Add database related env variables

--- apps/api/src/config/config.service.ts
+++ apps/api/src/config/config.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService as NestConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 import { Env, NodeEnv } from '.';
 
 @Injectable()
 export class ConfigService {
-    constructor(private env: NestConfigService<Env>) {}
+    constructor(private env: NestConfigService<Env, true>) {}
 
-    get<T>(key: keyof Env) {
+    get<T = any>(key: keyof Env) {
         return this.env.get<T>(key);
     }
 
@@ -21,5 +22,16 @@ export class ConfigService {
 
     get CLIENT_URL() {
         return this.env.get<string>('CLIENT_URL');
+    }
+
+    databaseCredentials(): TypeOrmModuleOptions {
+        return {
+            type: 'postgres',
+            url: this.get<string>('DATABASE_URL'),
+            ssl:
+                this.get<string>('DATABASE_SSL') === 'true'
+                    ? { requestCert: true, rejectUnauthorized: false }
+                    : undefined,
+        };
     }
 }
