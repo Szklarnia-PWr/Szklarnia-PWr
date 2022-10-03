fbf59438c3761dd77a8a7856d79d70b73768f7cb
JakubZojdzik <jakub.zojdzik@gmail.com>
2022-10-03 21:15:25 +0200

    Add `TimeSwitch`

--- apps/client/src/components/Statistics/Statistics.tsx
+++ apps/client/src/components/Statistics/Statistics.tsx
@@ -1,10 +1,11 @@
 import { Box } from '@mui/material';
 
-import { Charts, Highscores } from '../../components';
+import { Charts, Highscores, TimeSwitch } from '../../components';
 
 export const Statistics = () => {
     return (
         <Box sx={{ marginY: '5rem' }}>
+            <TimeSwitch />
             <Highscores />
             <Charts />
         </Box>
