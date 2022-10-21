b235984eb7d66a93431e009dd2158a3ebd0cf456
Jakub Żojdzik <69974199+JakubZojdzik@users.noreply.github.com>
2022-10-21 13:00:01 +0200

    Feature/64 block content without cookie (#65)
    * Add secret access cookie
    
    * Style in-work page

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -1,11 +1,20 @@
 import { Box } from '@mui/material';
+import { Container } from '@mui/system';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { Footer, Navbar, Thermometer } from '../components';
 import { Statistics } from '../components/Statistics';
 
 export const App = () => {
-    return (
+    const cookieVal = (document.cookie.match(
+        /^(?:.*;)?\s*nothink-interesting\s*=\s*([^;]+)(?:.*)?$/,
+    ) || [, null])[1];
+    return cookieVal !== 'V1cwMWMyRldhM2xqUjNocFltdFpNbHBITVVaUVVUMDk=' ? (
+        <Container sx={{ textAlign: 'center' }}>
+            <h1>TRWAJĄ PRACE!</h1>
+            <h2>Oficjalne otwarcie: (jeszcze nwm xd)</h2>
+        </Container>
+    ) : (
         <Box
             sx={{
                 display: 'flex',
